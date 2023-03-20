b4c2fbffa2c8cf3ba3e96989382d7a4475c3510a
GerritFischer <gerrit.fischer2@icloud.com>
2023-03-20 17:14:14 +0100

    Update image-sorter.py

--- image-sorter.py
+++ image-sorter.py
@@ -4,27 +4,14 @@ from PIL import Image, ImageTk
 from tkinter import filedialog
 import shutil
 import os
-img_list = []
-path_list = []
-
-
- 
-
-
-root = tk.Tk()
-root.title("Image Sorter")
-root.geometry("1280x720")
-
-
-
-
-path = ""
-movePath = ""
-
-imageLabel = ttk.Label(root, width=100)
-imageLabel.pack(side="left")
 
 counter = 0
+img_list = []
+path_list = []
+path = ""
+movePath = ""
+ 
+
 def updateImage():
     global counter
     counter += 1
@@ -32,9 +19,6 @@ def updateImage():
         imageLabel.configure(image=img_list[counter])
     except:
         resetProgram()
-
-        
-        
 
 def keep():
 
@@ -67,15 +51,12 @@ def loadDirectory():
         imageLabel.configure(image=img_list[0])
         moveButton.configure(state="normal")
         keepButton.configure(state="normal")
-
-    
-
+   
 def enableLoadButton():
     global path
     global movePath
     if path != "" and movePath != "" and path != "/" and movePath != "/" :
         loadDictButton.configure(state="normal")
-
 
 def selectPath():
      global path
@@ -88,11 +69,20 @@ def selectMove():
      movePath = filedialog.askdirectory()  + "/"
      moveLabel.configure(text="MovePath: " + movePath)
      enableLoadButton()
-def open_popup(title, text):
+
+def open_popup(title, text):      
    top= tk.Toplevel(root)
-   top.geometry("200x200")
+   top.geometry("200x100")
    top.title(title)
-   ttk.Label(top, text=text, font=('Mistral 18 bold')).place(x=10,y=10)
+   top.resizable(width = 0, height = 0)
+   def closeButton():
+       top.destroy()
+       top.update() 
+   ttk.Button(top, text="Okay", command=closeButton).pack(side="bottom")
+   ttk.Label(top, text=text, font=('Mistral 18 bold')).place(x=10, y=30)
+   x = root.winfo_x()
+   y = root.winfo_y()
+   top.geometry("+%d+%d" %(x+540,y+310))
 
 def resetProgram():
     global img_list
@@ -111,15 +101,21 @@ def onKeyPress(event):
         keep()
     elif event.char == "e":
         move()
+    elif event.char == "o":
+        open_popup("test", "test")
 
-#testdir = filedialog.askdirectory()
-#print(testdir)
+root = tk.Tk()
+root.title("Image Sorter")
+root.geometry("1280x720")
+root.resizable(height = 0, width = 0)
+
+imageLabel = ttk.Label(root, width=100)
+imageLabel.pack(side="left")
 moveLabel = ttk.Label(text="MovePath:  ", font=('Mistral 18 bold'))
 moveLabel.pack(side="bottom",expand=True, fill="x")
 pathLabel = ttk.Label(text="Path: ", font=('Mistral 18 bold'))
 pathLabel.pack(side="bottom", expand=True, fill="x")
-
-loadDictButton = ttk.Button(text="Load Directory", command=loadDirectory, state="disabled")
+loadDictButton = ttk.Button(text="Load Directory", command=loadDirectory, state="disabled", font=('Mistral 18 bold'))
 loadDictButton.pack(expand=True, fill="both")
 selectPathButton = ttk.Button(text="Select Path", command=selectPath)
 selectPathButton.pack(expand=True, fill="both")
